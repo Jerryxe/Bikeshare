8eba9a0c6e5fc385628b63f98fd6863dc1917cf3
Jie Jiang <Jerryxe@gmail.com>
2014-09-04 01:07:58 -0700

    Models with OOS

--- Code.R
+++ Code.R
@@ -23,19 +23,52 @@ library(plyr)
 training <- read.csv("train.csv", header = T, stringsAsFactors = F)
 training$DateTime <- strptime(training$datetime, format = "%Y-%m-%d %H:%M:%S")
 training$datetime <- NULL
+training <- training[order(training$DateTime), ]
 training$Hour <- factor(training$DateTime$hour)
 training$Weekday <- factor(weekdays(training$DateTime))
-training$t <- as.numeric(as.Date(training$DateTime) - as.Date(training$DateTime[1])) + 1
 training$season <- factor(training$season)
 training$holiday <- factor(training$holiday)
 training$workingday <- factor(training$workingday)
 training$weather <- factor(training$weather)
 training$Date <- as.Date(training$DateTime)
 training$ID <- 1:length(training$Date)
-training <- data.frame(training[, 17], training[, c(12, 16)], training[, 1:8], training[, 13:15], training[, 9:11])
-names(training) <- c("ID", "DateTime", "Date", "Season", "Holiday", "Workday", "Weather", "Temp", "ATemp", "Humidity", "WindSpeed", "Hour", "Weekday", "T", "Casual", "Registered", "Count")
+training$Ds <- as.numeric(as.Date(training$DateTime) - as.Date(training$DateTime[1])) + training$DateTime$hour/24
+training$Day <- factor(training$DateTime$mday)
+training <- data.frame(training[, 16], training[, 12], training[, 15], training[, 18], training[, 17], training[, 14], training[, 13], training[, 1:8], training[, 11])
+names(training) <- c("ID", "DateTime", "Date", "Day", "Ds", "Weekday", "Hour", "Season", "Holiday", "Workday", "Weather", "Temp", "ATemp", "Humidity", "WindSpeed", "Count")
 training <- training[order(training$DateTime), ]
 training$DateTime <- as.POSIXlt(training$DateTime)
+## Log Transformation
+training$CountLog <- log(training$Count)
+training$DsLog <- log(training$Ds + 0.001)
+training$TempLog <- log(training$Temp)
+training$ATempLog <- log(training$ATemp)
+training$HumidityLog <- log(training$Humidity + 0.001)
+training$WindSpeedLog <- log(training$WindSpeed + 0.001)
+
+## Split into Train and Test data sets, days between 1st and 15th will be in Train, and days between 16th and 19th will be in Test
+Train <- training[training$Day %in% as.character(1:15), ]
+Test <- training[training$Day %in% as.character(16:19), ]
+Test$ActualCount <- Test$Count
+Test$ActualCountLog <- Test$CountLog
+Test$Count <- rep(0, nrow(Test))
+Test$CountLog <- rep(0, nrow(Test))
+Test$HLag1CountLog <- rep(0, nrow(Test))
+Test$HLag1Delay <- rep(0, nrow(Test))
+Test$HLag1Int <- rep(0, nrow(Test))
+Test$HLag2CountLog <- rep(0, nrow(Test))
+Test$HLag2Delay <- rep(0, nrow(Test))
+Test$HLag2Int <- rep(0, nrow(Test))
+Test$HLag3CountLog <- rep(0, nrow(Test))
+Test$HLag3Delay <- rep(0, nrow(Test))
+Test$HLag3Int <- rep(0, nrow(Test))
+Test$HLag4CountLog <- rep(0, nrow(Test))
+Test$HLag4Delay <- rep(0, nrow(Test))
+Test$HLag4Int <- rep(0, nrow(Test))
+Test$HLag5CountLog <- rep(0, nrow(Test))
+Test$HLag5Delay <- rep(0, nrow(Test))
+Test$HLag5Int <- rep(0, nrow(Test))
+
 
 # Evaluation Formula (Root Mean Squared Logarithmic Error):
 RMSLE <- function(Predicted, Reference) {
@@ -45,101 +78,194 @@ RMSLE <- function(Predicted, Reference) {
 
 # Exploratory Analysis
 ## Plot Autocorrelation Function (ACF), and we could see:
-## 1. There is autocorrelations for about 5 periods (will dig deeper).
-## 2. There is alternative dependence with about 24 periods interval (presumably means 24 hours).
-plot(acf(training$Count, lag.max = 100))
+## There is alternative dependence (seasonality) with about 24 periods interval (presumably means 24 hours).
+acf(Train$Count, lag.max = 100)
 
-## Plot ACF for each hour:
-## 1. Now we could see there is alternative dependence for about 7 periods (presumably means 7 days in a week).
-plot(acf(training$Count[training$Hour == 12], lag.max= 50))
+## Plot ACF for 24 hours:
+## Now we could see there is alternative dependence (seasonality) for about 7 periods (presumably means 7 days in a week).
+acf(Train$Count[Train$Hour == 8], lag.max= 50)
 
-## Plot ACF for both hour and weekday, and here we check few different day and hour:
-## 1. There is almost no alternative dependence
-plot(acf(training$Count[training$Hour == 12 & training$Weekday == "Saturday"]))
-plot(acf(training$Count[training$Hour == 16 & training$Weekday == "Tuesday"]))
+## Plot ACF for both 24 hour and 7 days in a week, and here we check few different day and hour:
+## 1. There is almost no alternative dependence (seasonality)
+## 2. There are about 5 lags that are above significance boundaries
+acf(Train$Count[Train$Hour == 16 & Train$Weekday == "Tuesday"])
+acf(Train$Count[Train$Hour == 8 & Train$Weekday == "Sunday"])
+
+## Plot ACF for weekly seasonality, and we could see there is 4-6 week lags
+Temp <- ddply(Train, .(Date, Weekday), summarize, Count = sum(Count))
+acf(Temp[Temp$Weekday == "Monday", ]$Count)
+acf(Temp[Temp$Weekday == "Tuesday", ]$Count)
+acf(Temp[Temp$Weekday == "Wednesday", ]$Count)
+acf(Temp[Temp$Weekday == "Thursday", ]$Count)
+acf(Temp[Temp$Weekday == "Friday", ]$Count)
+acf(Temp[Temp$Weekday == "Saturday", ]$Count)
+acf(Temp[Temp$Weekday == "Sunday", ]$Count)
 
 
 ## Plot the cycle of 24 hours
 ### There is significant difference in each hour
-ggplot(training, aes(x = Hour, y = Count)) + geom_boxplot(aes(fill = Hour)) + scale_fill_hue(guide = F)
-### Plot the Jan 2011 rent, and we could see there is claer cycle
-ggplot(training[training$DateTime$year == 111 & training$DateTime$mon == 0, ], aes(x = DateTime, y = Count)) + geom_point() + geom_line()
-### Plot the first 5 days of Jan 2011 to have a closer look, and we could see for each day, there are 2 peaks (around 8AM and 6PM), and there is also a low time at the end of each day.
-ggplot(training[training$DateTime$year == 111 & training$DateTime$mon == 0 & training$DateTime$yday <= 5, ], aes(x = DateTime, y = Count)) + geom_point() + geom_line()
+ggplot(Train, aes(x = Hour, y = Count)) + geom_boxplot(aes(fill = Hour)) + scale_fill_hue(guide = F)
+### Plot the Jan 2011 rent, and we could see there is claer seasonality
+ggplot(Train[Train$DateTime$year == 111 & Train$DateTime$mon == 0, ], aes(x = DateTime, y = Count)) + geom_point() + geom_line()
+### Plot the first 5 days of Jan 2011 to have a closer look, and we could see for each day, there are 2 peaks (around 8AM and 6PM), and there is also one trough at the end of each day.
+ggplot(Train[Train$DateTime$year == 111 & Train$DateTime$mon == 0 & Train$DateTime$yday <= 5, ], aes(x = DateTime, y = Count)) + geom_point() + geom_line()
+### Plot the count for each of the day in a month, and we do not see a pattern, so we do not need to consider monthly seasonality.
+ggplot(Train, aes(x = factor(Day), y = Count)) + geom_boxplot(aes(fill = factor(Day))) + scale_fill_hue(guide = F)
 
-
-## Plot the cycle of 7 days in a week
+## Plot the weekly seasonality
 ### Plot the rent of 8AM for each different weekday. Apparently, there is significant different between weekdays and weekends.
-ggplot(training[training$Hour == 8, ], aes(x = DateTime, y = Count)) + geom_line(aes(color = Weekday))
+ggplot(Train[Train$Hour == 8, ], aes(x = DateTime, y = Count)) + geom_line(aes(color = Weekday))
 ### Workday plot also shows this pattern.
-ggplot(training[training$Hour == 8, ], aes(x = DateTime, y = Count)) + geom_line(aes(color = Workday))
+ggplot(Train[Train$Hour == 8, ], aes(x = DateTime, y = Count)) + geom_line(aes(color = Workday))
 
 
-# Preapring T-Series Data
-## Add T-1, T-2, T-3 Count Data based on Weekday and Hour
-WeekdayList <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
-HourList <- as.character(0:23)
-training2 <- training[0, ]
-for (i in 1:7) {
-    for (j in 1:24) {
-        Temp <- training[training$Weekday == WeekdayList[i] & training$Hour == HourList[j], ]
-        Temp$Tm1Count <- c(NA, Temp$Count[1:(length(Temp$Count) - 1)])
-        Temp$Tm1Delay <- c(NA, Temp$T[2:length(Temp$T)] - Temp$T[1:(length(Temp$T) - 1)])/7
-        Temp$Tm2Count <- c(NA, NA, Temp$Count[1:(length(Temp$Count)-2)])
-        Temp$Tm2Delay <- c(NA, NA, Temp$T[3:length(Temp$T)] - Temp$T[1:(length(Temp$T) - 2)])/7
-        Temp$Tm3Count <- c(NA, NA, NA, Temp$Count[1:(length(Temp$Count)-3)])
-        Temp$Tm3Delay <- c(NA, NA, NA, Temp$T[4:length(Temp$T)] - Temp$T[1:(length(Temp$T) - 3)])/7
-        training2 <- rbind(training2, Temp)
-    }
-}
-training <- training2[order(training2$ID), ]
-training$Tm1Mix <- training$Tm1Count * training$Tm1Delay
-training$Tm2Mix <- training$Tm2Count * training$Tm2Delay
-training$Tm3Mix <- training$Tm3Count * training$Tm3Delay
-rm(training2, i, j)
+## Plot Month day, so significant difference
+ggplot(Train[Train$Day %in% c("1", "5", "10", "15") & Train$Date < as.Date("2012-01-01"), ], aes(x = DateTime, y = Count)) + geom_line(aes(color = Day))
+
+
+
+## Add 5 lags in hour and do log transformation
+Train$HLag1CountLog <- log(c(NA, Train$Count[1:(length(Train$Count) - 1)]))
+Train$HLag1Delay <- c(NA, Train$Ds[2:length(Train$Ds)] - Train$Ds[1:(length(Train$Ds) - 1)])
+Train$HLag1Delay <- ifelse(Train$HLag1Delay > 1, 0, Train$HLag1Delay * 24)
+Train$HLag1CountLog <- ifelse(Train$HLag1Delay == 0, 0, Train$HLag1CountLog)
+Train$HLag1Int <- ifelse(Train$HLag1Delay == 0, 0, Train$HLag1CountLog / Train$HLag1Delay)
+Train$HLag2CountLog <- log(c(NA, NA, Train$Count[1:(length(Train$Count) - 2)]))
+Train$HLag2Delay <- c(NA, NA, Train$Ds[3:length(Train$Ds)] - Train$Ds[1:(length(Train$Ds) - 2)])
+Train$HLag2Delay <- ifelse(Train$HLag2Delay > 1, 0, Train$HLag2Delay * 24)
+Train$HLag2CountLog <- ifelse(Train$HLag2Delay == 0, 0, Train$HLag2CountLog)
+Train$HLag2Int <- ifelse(Train$HLag2Delay == 0, 0, Train$HLag2CountLog / Train$HLag2Delay)
+Train$HLag3CountLog <- log(c(NA, NA, NA, Train$Count[1:(length(Train$Count) - 3)]))
+Train$HLag3Delay <- c(NA, NA, NA, Train$Ds[4:length(Train$Ds)] - Train$Ds[1:(length(Train$Ds) - 3)])
+Train$HLag3Delay <- ifelse(Train$HLag3Delay > 1, 0, Train$HLag3Delay * 24)
+Train$HLag3CountLog <- ifelse(Train$HLag3Delay == 0, 0, Train$HLag3CountLog)
+Train$HLag3Int <- ifelse(Train$HLag3Delay == 0, 0, Train$HLag3CountLog / Train$HLag3Delay)
+Train$HLag4CountLog <- log(c(NA, NA, NA, NA, Train$Count[1:(length(Train$Count) - 4)]))
+Train$HLag4Delay <- c(NA, NA, NA, NA, Train$Ds[5:length(Train$Ds)] - Train$Ds[1:(length(Train$Ds) - 4)])
+Train$HLag4Delay <- ifelse(Train$HLag4Delay > 1, 0, Train$HLag4Delay * 24)
+Train$HLag4CountLog <- ifelse(Train$HLag4Delay == 0, 0, Train$HLag4CountLog)
+Train$HLag4Int <- ifelse(Train$HLag4Delay == 0, 0, Train$HLag4CountLog / Train$HLag4Delay)
+Train$HLag5CountLog <- log(c(NA, NA, NA, NA, NA, Train$Count[1:(length(Train$Count) - 5)]))
+Train$HLag5Delay <- c(NA, NA, NA, NA, NA, Train$Ds[6:length(Train$Ds)] - Train$Ds[1:(length(Train$Ds) - 5)])
+Train$HLag5Delay <- ifelse(Train$HLag5Delay > 1, 0, Train$HLag5Delay * 24)
+Train$HLag5CountLog <- ifelse(Train$HLag5Delay == 0, 0, Train$HLag5CountLog)
+Train$HLag5Int <- ifelse(Train$HLag5Delay == 0, 0, Train$HLag5CountLog / Train$HLag5Delay)
+
+
+## Number of non-NA observations: 8595/8600.
+sum(complete.cases(Train$HLag1Int, Train$HLag2Int, Train$HLag3Int, Train$HLag4Int, Train$HLag5Int))
 
 
 # Data quality check
-## Holiday is a near zero variance, but the rest are not, so the quality is decent
-nearZeroVar(training[, c(4:13, 15:26)], saveMetrics = T)
-## Temp is highly correlated with ATemp, Registered is highly correlated with Count.
-round(cor(training[, c(8:11, 15:17)]), 2)
-findCorrelation(cor(training[, c(8:11, 15:17)]), cutoff = 0.75, verbose = T)
+## Holiday and HLagDelay are near zero variance variables, so the quality is decent
+nearZeroVar(Train[, c(8:37)], saveMetrics = T)
+## Temp is highly correlated with ATemp.
+round(cor(Train[, c(12:16)]), 2)
+findCorrelation(cor(Train[, c(12:16)]), cutoff = 0.75, verbose = T)
 ## No linear combination is found
-findLinearCombos(training[, c(8:11, 18:26)][complete.cases(training[, c(24:26)]), ])
+findLinearCombos(Train[, c(12:37)][complete.cases(Train[, c(12:37)]), ])
 
 
-# Build models with interactions and 3-period autocorrelation AR(3)
-Formula <- formula(Count ~ Season + Holiday + Workday + Weather + Temp + ATemp + Humidity + WindSpeed + Hour + Weekday + Tm1Count + Tm1Delay + Tm1Mix + Tm2Count + Tm2Delay + Tm2Mix + Tm3Count + Tm3Delay + Tm3Mix)
-trainingTm3 <- training[complete.cases(training[, c("Tm1Mix", "Tm2Mix", "Tm3Mix")]), ]
+# Build models
+Formula <- formula(CountLog ~ DsLog + Hour + Weekday + Season + Holiday + Workday + Weather + TempLog + ATempLog + HumidityLog + WindSpeedLog + HLag1CountLog + HLag1Delay + HLag1Int + HLag2CountLog + HLag2Delay + HLag2Int + HLag3CountLog + HLag3Delay + HLag3Int + HLag4CountLog + HLag4Delay + HLag4Int + HLag5CountLog + HLag5Delay + HLag5Int)
+TrainH5 <- Train[complete.cases(Train[, c("HLag1Int", "HLag2Int", "HLag3Int", "HLag4Int", "HLag5Int")]), ]
 
-## Linear gaussian model. In-sample performance: R2 is 88%, and RMSLE is 0.76
-lmModel <- train(Formula, data = trainingTm3, method = "lm")
-lmPred <- predict(lmModel, newdata = trainingTm3)
-lmPred <- ifelse(lmPred < 0, 0, round(qrPred))
-(lmRMSLE <- RMSLE(lmPred, trainingTm3[, "Count"]))
-### From the residual plot we could see there is non-constant variance in the residual, so we need to do the transformation.
-ggplot(NULL, aes(x = lmPred, y = trainingTm3[, "Count"] - lmPred)) + geom_point()
+## User-Defined Prediction Function
+PREDICT <- function(Model) {
+    FullTraining <- rbind(data.frame(Train, Set = rep("Train", nrow(Train))), data.frame(Test[, c(1:22, 25:39)], Set = rep("Test", nrow(Test))))
+    FullTraining <- FullTraining[order(FullTraining$ID), ]
+    for (i in 6:nrow(FullTraining)) {
+        if (FullTraining[i, "Set"] == "Test") {
+            ## fill lag 1
+            if((FullTraining[i, "Ds"] - FullTraining[i - 1, "Ds"]) < 1) {
+                FullTraining[i, "HLag1CountLog"] <- FullTraining[i - 1, "CountLog"]
+                FullTraining[i, "HLag1Delay"] <- (FullTraining[i, "Ds"] - FullTraining[i - 1, "Ds"]) * 24
+                FullTraining[i, "HLag1Int"] <- FullTraining[i, "HLag1CountLog"] / FullTraining[i, "HLag1Delay"]
+            } else {
+                FullTraining[i, "HLag1CountLog"] <- 0
+                FullTraining[i, "HLag1Delay"] <- 0
+                FullTraining[i, "HLag1Int"] <- 0
+            }
+            ## fill lag 2
+            if((FullTraining[i, "Ds"] - FullTraining[i - 2, "Ds"]) < 1) {
+                FullTraining[i, "HLag2CountLog"] <- FullTraining[i - 2, "CountLog"]
+                FullTraining[i, "HLag2Delay"] <- (FullTraining[i, "Ds"] - FullTraining[i - 2, "Ds"]) * 24
+                FullTraining[i, "HLag2Int"] <- FullTraining[i, "HLag2CountLog"] / FullTraining[i, "HLag2Delay"]
+            } else {
+                FullTraining[i, "HLag2CountLog"] <- 0
+                FullTraining[i, "HLag2Delay"] <- 0
+                FullTraining[i, "HLag2Int"] <- 0
+            }
+            ## fill lag 3
+            if((FullTraining[i, "Ds"] - FullTraining[i - 3, "Ds"]) < 1) {
+                FullTraining[i, "HLag3CountLog"] <- FullTraining[i - 3, "CountLog"]
+                FullTraining[i, "HLag3Delay"] <- (FullTraining[i, "Ds"] - FullTraining[i - 3, "Ds"]) * 24
+                FullTraining[i, "HLag3Int"] <- FullTraining[i, "HLag3CountLog"] / FullTraining[i, "HLag3Delay"]
+            } else {
+                FullTraining[i, "HLag3CountLog"] <- 0
+                FullTraining[i, "HLag3Delay"] <- 0
+                FullTraining[i, "HLag3Int"] <- 0
+            }
+            ## fill lag 4
+            if((FullTraining[i, "Ds"] - FullTraining[i - 4, "Ds"]) < 1) {
+                FullTraining[i, "HLag4CountLog"] <- FullTraining[i - 3, "CountLog"]
+                FullTraining[i, "HLag4Delay"] <- (FullTraining[i, "Ds"] - FullTraining[i - 4, "Ds"]) * 24
+                FullTraining[i, "HLag4Int"] <- FullTraining[i, "HLag4CountLog"] / FullTraining[i, "HLag4Delay"]
+            } else {
+                FullTraining[i, "HLag4CountLog"] <- 0
+                FullTraining[i, "HLag4Delay"] <- 0
+                FullTraining[i, "HLag4Int"] <- 0
+            }
+            ## fill lag 5
+            if((FullTraining[i, "Ds"] - FullTraining[i - 5, "Ds"]) < 1) {
+                FullTraining[i, "HLag5CountLog"] <- FullTraining[i - 3, "CountLog"]
+                FullTraining[i, "HLag5Delay"] <- (FullTraining[i, "Ds"] - FullTraining[i - 5, "Ds"]) * 24
+                FullTraining[i, "HLag5Int"] <- FullTraining[i, "HLag5CountLog"] / FullTraining[i, "HLag5Delay"]
+            } else {
+                FullTraining[i, "HLag5CountLog"] <- 0
+                FullTraining[i, "HLag5Delay"] <- 0
+                FullTraining[i, "HLag5Int"] <- 0
+            }
+            FullTraining[i, "CountLog"] <- predict(Model, FullTraining[i, ])
+            FullTraining[i, "Count"] <- exp(FullTraining[i, "CountLog"])
+        }
+    }
+    return (FullTraining[FullTraining$Set == "Test", "Count"])
+}
 
-### With the log transformation of Count, Tm1Count, Tm2Count, Tm3Count, RMSLE is reduced to 0.38 from 0.76
-trainingTm3$Tm1CountLog <- log(trainingTm3$Tm1Count)
-trainingTm3$Tm2CountLog <- log(trainingTm3$Tm2Count)
-trainingTm3$Tm3CountLog <- log(trainingTm3$Tm3Count)
-trainingTm3$Tm1MixLog <- trainingTm3$Tm1CountLog * trainingTm3$Tm1Delay
-trainingTm3$Tm2MixLog <- trainingTm3$Tm2CountLog * trainingTm3$Tm2Delay
-trainingTm3$Tm3MixLog <- trainingTm3$Tm3CountLog * trainingTm3$Tm3Delay
-lm2Model <- train(log(Count) ~ Season + Holiday + Workday + Weather + Temp + ATemp + Humidity + WindSpeed + Hour + Weekday + Tm1CountLog + Tm1Delay + Tm1MixLog + Tm2CountLog + Tm2Delay + Tm2MixLog + Tm3CountLog + Tm3Delay + Tm3MixLog, data = trainingTm3, method = "lm")
-lm2Pred <- predict(lm2Model, newdata = trainingTm3)
-(lm2RMSLE <- RMSLE(round(exp(lm2Pred)), trainingTm3[, "Count"]))
-### Now the residuals have constant variance
-ggplot(NULL, aes(x = lm2Pred, y = log(trainingTm3$Count) - lm2Pred)) + geom_point()
+
+## Full Model, Adj R-square: 92.9%, IS RMSLE: 0.358, OOS RMSLE: 0.6
+fullModel <- lm(Formula, data = TrainH5)
+### In-Sample Prediction and RMSLE
+fullPredIS <- predict(fullModel, newdata = TrainH5)
+(fullRMSLEIS <- RMSLE(exp(fullPredIS), TrainH5[, "Count"]))
+ggplot(NULL, aes(y = fullPredIS, x = TrainH5[, "CountLog"])) + geom_point()
+### Out-of-Sample Prediction and RMSLE
+fullPredOOS <- PREDICT(fullModel)
+(fullRMSLEOOS <- RMSLE(fullPredOOS, Test$ActualCount))
+ggplot(NULL, aes(y = log(fullPredOOS), x = Test[, "ActualCountLog"])) + geom_point()
+
+
+## Stepwise, IS RMSLE: 0.358, OOS RMSLE: 0.6, exactly the same as full model (means the majority of the variables are significant, and step model is very similar to full model)
+nullModel <- lm(CountLog ~ 1, data = TrainH5)
+stepModel <- step(nullModel, scope = Formula, dir = "both")
+### In-Sample Prediction and RMSLE
+stepPredIS <- predict(stepModel, newdata = TrainH5)
+(stepRMSLEIS <- RMSLE(exp(stepPredIS), TrainH5[, "Count"]))
+ggplot(NULL, aes(y = stepPredIS, x = TrainH5[, "CountLog"])) + geom_point()
+### Out-of-Sample Prediction and RMSLE
+stepPredOOS <- PREDICT(stepModel)
+(stepRMSLEOOS <- RMSLE(stepPredOOS, Test$ActualCount))
+ggplot(NULL, aes(y = log(stepPredOOS), x = Test[, "ActualCountLog"])) + geom_point()
 
 
 
-## poisson model. It didn't work well, RMSLE is 3.15
-poisModel <- glm(Formula, data = trainingTm3, family = "poisson")
-poisPred <- predict(poisModel, newdata = trainingTm3)
-qrRMSLE <- RMSLE(round(poisPred), trainingTm3[, "Count"])
+
+
+
+################### Do not run codes under this line ######################
+
+
 
 ## Lasso model. RMSLE is 0.68, better than gaussian model
 trainingTm3Lasso <- model.matrix(Formula, data = trainingTm3)[, -1]
@@ -155,5 +281,6 @@ lassoCVPred <- predict(lassoCVModel, newx = trainingTm3Lasso)
 lassoCVPred <- as.vector(lassoCVPred)
 lassoCVPred <- ifelse(lassoCVPred < 0, 0, round(lassoCVPred))
 lassoCVRMSLE <- RMSLE(lassoCVPred, trainingTm3[, "Count"])
+
 
 
